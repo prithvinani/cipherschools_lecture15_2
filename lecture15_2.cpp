6cecba40821f473aa1baacd623855a6a72b03f3a
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-07-08 04:31:16 +0530

    Add files via upload

---
+++ lecture15_2.cpp
@@ -0,0 +1,32 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string name;
+//     cin>>name;
+//     int i;
+//     for (i = 0; i < 5; i++)
+//     {
+//         cout<<name[i]<<endl;
+//     }
+//     return 0;
+// }
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string a;
+    string b;
+    string c;
+    // int a;
+    // int b;
+    // int c;
+    // cin>>a;
+    getline(cin,a);
+    cout<<"your full name is "<<a<<endl;
+    // c=a+b;
+    // cout<<"the combined word is: "<<c;
+    // cout<<"the lenght of the comnbined name is "<<c.length();
+    return 0;
+}
